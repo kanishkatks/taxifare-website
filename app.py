abdd98dc8f2562f211447ee4765f1dc292d246cb
Kanishk Waghmare <kanishkatks@gmail.com>
2025-03-07 16:14:41 +0100

    My first website

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from geopy.geocoders import Nominatim
 
 st.markdown(f"<h1 align = 'center' style='font-size: 48px;'>TaxiFarePred</h1>",unsafe_allow_html=True)
 
@@ -15,16 +16,21 @@ time_ =st.time_input(label='Time of pickup',)
 
 with col1:
     st.write('Pick up location')
-    p_lon = st.number_input(label='Enter the pickup longitude', min_value=-74.7,max_value=-74.3)
-    p_lat = st.number_input(label= 'Enter the pickup latitude', min_value =40.5, max_value=40.9)
+    pick_add = st.text_input(label = 'Enter the pickup address')
+    # p_lon = st.number_input(label='Enter the pickup longitude', min_value=-74.7,max_value=-74.3)
+    # p_lat = st.number_input(label= 'Enter the pickup latitude', min_value =40.5, max_value=40.9)
 
 with col2:
     st.write('Drop up location')
-    d_lon =st.number_input(label='Enter the dropoff longtitude', min_value=-74.7,max_value=-74.3)
-    d_lat = st.number_input(label= 'Enter the dropoff latitude', min_value =40.5, max_value=40.9)
+    drop_add = st.text_input(label = 'Enter the DropOff address')
+    # d_lon =st.number_input(label='Enter the dropoff longtitude', min_value=-74.7,max_value=-74.3)
+    # d_lat = st.number_input(label= 'Enter the dropoff latitude', min_value =40.5, max_value=40.9)
 
 pass_count = st.select_slider('Number of people:', ['1','2', '3', '4','5'])
 
+geolocator = Nominatim(user_agent="geoapi_explorer")  # Set a unique user agent
+pick_loc = geolocator.geocode(pick_add)
+drop_loc = geolocator.geocode(drop_add)
 
 
 
@@ -38,10 +44,10 @@ if url == 'https://taxifare.lewagon.ai/predict':
 #2.
 params = {
     'pickup_datetime': f'{date_} {time_}',
-    'pickup_longitude': p_lon,
-    'pickup_latitude': p_lat,
-    'dropoff_longitude': d_lon,
-    'dropoff_latitude': d_lat,
+    'pickup_longitude': pick_loc.longitude ,
+    'pickup_latitude':  pick_loc.latitude,
+    'dropoff_longitude': drop_loc.longitude,
+    'dropoff_latitude': drop_loc.latitude,
     'passenger_count': pass_count,
 }
 import requests
